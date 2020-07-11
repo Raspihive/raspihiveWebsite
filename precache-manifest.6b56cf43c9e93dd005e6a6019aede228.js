e7387f40b43950b97e388eed94ae103edc7cac05
Cryptec <neupi92@web.de>
2020-07-11 18:16:10 +0200

    Updates

---
+++ precache-manifest.6b56cf43c9e93dd005e6a6019aede228.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5030bfeb8731afaa8e6979e3682d92a4",
+    "url": "/Cryptec/raspihiveWebsite/settings/index.html"
+  },
+  {
+    "revision": "6f1e1866ae400cad554e",
+    "url": "/Cryptec/raspihiveWebsite/settings/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f05140522fd4a5ef65a",
+    "url": "/Cryptec/raspihiveWebsite/settings/static/js/2.7fa9e3c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cryptec/raspihiveWebsite/settings/static/js/2.7fa9e3c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1e1866ae400cad554e",
+    "url": "/Cryptec/raspihiveWebsite/settings/static/js/main.30fc8c65.chunk.js"
+  },
+  {
+    "revision": "e3c5c3a5697ed05abfd8",
+    "url": "/Cryptec/raspihiveWebsite/settings/static/js/runtime-main.7f06898c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Cryptec/raspihiveWebsite/settings/static/media/logo.5d5d9eef.svg"
+  }
+]);
