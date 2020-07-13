82e9ed82550841146ef555377d29a640e3dc81c4
Cryptec <neupi92@web.de>
2020-07-13 16:35:14 +0200

    change landingpage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Logo from './logo';
 import './App.css';
 
+
+
 function App() {
   return (
    <div >
       <header className="App-header">
          <Logo />
       </header>
+      <body className="App-body"> 
+      <div className="infobox">
+      <text>100% Open Source! Yes, you read that right! Raspihive is built and run for the community, by the community.</text>
+      <text>So that saying really is correct... The best things in life are free!</text>
+      </div>
+      </body>
    </div>
     
     
