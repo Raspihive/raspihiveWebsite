fc59f9d7c09042ffb358e5cb8165def87d4c24b0
Cryptec <neupi92@web.de>
2020-07-14 11:08:47 +0200

    ✔️ added Navbar + Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import Logo from './logo';
+import React from "react";
 import './App.css';
+import Homescreen from './Screens/home'
+import Downloadscreen from './Screens/download'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-
-
-function App() {
+export default function Navigation() {
   return (
-   <div >
-      <header className="App-header">
-         <Logo />
-      </header>
-      <body className="App-body"> 
-      <div className="infobox">
-      <text>100% Open Source! Yes, you read that right! Raspihive is built and run for the community, by the community.</text>
-      <text>So that saying really is correct... The best things in life are free!</text>
+    <Router >
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/download">Download</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/download">
+            <Download />
+          </Route>
+        </Switch>
       </div>
-      </body>
-   </div>
-    
-    
+    </Router>
   );
 }
 
-export default App;
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+       <Homescreen />
+    </div>
+  );
+}
+
+function Download() {
+  return (
+    <div>
+        <Downloadscreen />
+   </div>
+  );
+}
+
+function About() {
+   return (
+     <div>
+       <h2>About</h2>
+     </div>
+   );
+ }
+ 
---
+++ src/Screens/download.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../logo';
+import '../App.css';
+
+
+
+function Downloadscreen() {
+  return (
+   <div >
+      <header className="App-header">
+      <text>Download</text>
+      </header>
+      <body className="App-body"> 
+      </body>
+   </div>
+    
+    
+  );
+}
+
+export default Downloadscreen;
