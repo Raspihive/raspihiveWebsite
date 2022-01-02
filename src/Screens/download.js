31e838a3f8e14c4038f500d431454c39184e3e51
Cryptec <neupi92@web.de>
2022-01-02 13:23:39 +0100

    added v2.4.3

--- src/Screens/download.js
+++ src/Screens/download.js
@@ -22,7 +22,13 @@ const Download = () => {
           <br></br>
           <br></br>
           <P>
-            Version 2.1 | 2021-03-08 (recommended)
+            Version 2.4.3 | 2022-01-02 (recommended)
+          </P>
+          <button id="downloadButton" style={{ width: 150, height: 40 }}><a href="https://drive.google.com/file/d/13R5wPVXcwNzxmZu6B3pMQBozzj43J4Zj/view?usp=sharing">Download</a></button>
+          <br></br>
+          <br></br>
+          <P>
+            Version 2.1 | 2021-03-08
           </P>
           <button id="downloadButton" style={{ width: 150, height: 40 }}><a href="https://drive.google.com/u/0/uc?export=download&confirm=2VJS&id=1F6y7l_AqkaoPYWtxgWUVmD9wBqTEo00R">Download</a></button>
           <br></br>
